1bbaaf3e95ad8f11541aa012fa8d2be46b3d7c61
GEETANJALI083 <99342915+GEETANJALI083@users.noreply.github.com>
2022-02-12 22:32:38 +0530

    Add files via upload

---
+++ week1_q1 (1).c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+printf("Name:GEETANJALI BEHERA\n");
+printf("Regd no.:2102050083\n");
+printf("Branch: Electrical Engeneering\n");
+printf("Hobbies: Painting");
+getchar();
+}
