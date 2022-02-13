58e82cdd2262edf22673bf6a75aaab7e1db735a1
GEETANJALI083 <99342915+GEETANJALI083@users.noreply.github.com>
2022-02-13 09:51:10 +0530

    Add files via upload

---
+++ week1_q2 (1).c
@@ -0,0 +1,12 @@
+#include<math.h>
+#include<stdio.h>
+void main()
+{
+int a,b;
+printf("Enter two number\n");
+scanf("%d%d , &a, &b");
+if(a>b)
+printf("a is greater");
+else;
+printf("b is greater");
+}
---
+++ week1q1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    //prgrm to display basic information
+    char a[100],c[100],d[100];
+    int b;
+    printf("Name:Geetanjali Behera\n");
+    scanf("%s",&a);
+    printf("Registration no.:2102050083 \n");
+    scanf("%d",&b);
+    printf("Branch:Electrical engineering \n");
+    scanf("%s",&c);
+    printf("Hobbies: painting\n");
+    scanf("%s",&d);
+    return 0;
+}
+
