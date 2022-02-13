d8dabf41479f9a3296a9655c0279c5a1bcdc4424
GEETANJALI083 <99342915+GEETANJALI083@users.noreply.github.com>
2022-02-13 09:19:03 +0530

    Delete week1_q1 (1).c

--- week1_q1 (1).c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-printf("Name:GEETANJALI BEHERA\n");
-printf("Regd no.:2102050083\n");
-printf("Branch: Electrical Engeneering\n");
-printf("Hobbies: Painting");
-getchar();
-}
